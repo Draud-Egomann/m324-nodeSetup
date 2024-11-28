0b46fdadd6d18d50cb0b8fc5e2b27ca3a610aa70
Justin <justin-urbanek@bluewin.ch>
2024-11-28 10:21:58 +0100

    feat: code coverage with decimal numbers

--- index.test.js
+++ index.test.js
@@ -5,4 +5,16 @@ describe("sum", () => {
   it("should return 3 for 1 + 2", () => {
     assert.equal(sum(1, 2), 3);
   });
+
+  it("should return 1.1 for 0.5 + 0.6", () => {
+    assert.equal(sum(0.5, 0.6), 1.1);
+  });
+
+  it("should return 0 for -1 + 1", () => {
+    assert.equal(sum(-1, 1), 0);
+  });
+
+  it("should return -3 for -1 + -2", () => {
+    assert.equal(sum(-1, -2), -3);
+  });
 });
